9342ca34ec672e02430763c9772938adfee18b60
meenchen <meenchen79@gmail.com>
2023-05-30 12:01:46 +0800

    precommit and attention

--- transformer/include/common.h
+++ transformer/include/common.h
@@ -1,6 +1,7 @@
 #ifndef COMMON_H
 #define COMMON_H
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <stdexcept>
 
@@ -9,61 +10,99 @@
 #define DEBUG false
 #define USE_OPT_EXP
 
-#define DEBUG_INS(x) \
-    if (DEBUG) x
+#define DEBUG_INS(x)                                                           \
+  if (DEBUG)                                                                   \
+  x
 
-template <typename T>
-class Matrix3D {
-   public:
-    Matrix3D(T* data, int dim_x, int dim_y, int dim_z) : m_data(data), m_dim_x(dim_x), m_dim_y(dim_y), m_dim_z(dim_z) {}
+template <typename T> class Matrix3D {
+public:
+  Matrix3D(T *data, int dim_x, int dim_y, int dim_z)
+      : m_data(data), m_dim_x(dim_x), m_dim_y(dim_y), m_dim_z(dim_z) {}
 
-    T& operator()(int x, int y, int z) {
-        if (x < 0 || x >= m_dim_x || y < 0 || y >= m_dim_y || z < 0 || z >= m_dim_z) {
-            printf("%d, %d, %d\n", x, y, z);
-            printf("%d, %d, %d\n", m_dim_x, m_dim_y, m_dim_z);
-            throw std::out_of_range("Matrix3D: Indices out of range.");
-        }
-        return m_data[x * m_dim_y * m_dim_z + y * m_dim_z + z];
+  T &operator()(int x, int y, int z) {
+    if (x < 0 || x >= m_dim_x || y < 0 || y >= m_dim_y || z < 0 ||
+        z >= m_dim_z) {
+      printf("%d, %d, %d\n", x, y, z);
+      printf("%d, %d, %d\n", m_dim_x, m_dim_y, m_dim_z);
+      throw std::out_of_range("Matrix3D: Indices out of range.");
+    }
+    return m_data[x * m_dim_y * m_dim_z + y * m_dim_z + z];
+  }
+
+  const T &operator()(int x, int y, int z) const {
+    if (x < 0 || x >= m_dim_x || y < 0 || y >= m_dim_y || z < 0 ||
+        z >= m_dim_z) {
+      printf("%d, %d, %d\n", x, y, z);
+      printf("%d, %d, %d\n", m_dim_x, m_dim_y, m_dim_z);
+      throw std::out_of_range("Matrix3D: Indices out of range.");
+    }
+    return m_data[x * m_dim_y * m_dim_z + y * m_dim_z + z];
+  }
+
+  bool operator==(const Matrix3D<T> &other) const {
+    if (m_dim_x != other.m_dim_x || m_dim_y != other.m_dim_y ||
+        m_dim_z != other.m_dim_z) {
+      return false;
     }
 
-    const T& operator()(int x, int y, int z) const {
-        if (x < 0 || x >= m_dim_x || y < 0 || y >= m_dim_y || z < 0 || z >= m_dim_z) {
-            printf("%d, %d, %d\n", x, y, z);
-            printf("%d, %d, %d\n", m_dim_x, m_dim_y, m_dim_z);
-            throw std::out_of_range("Matrix3D: Indices out of range.");
-        }
-        return m_data[x * m_dim_y * m_dim_z + y * m_dim_z + z];
-    }
-
-    bool operator==(const Matrix3D<T>& other) const {
-        if (m_dim_x != other.m_dim_x || m_dim_y != other.m_dim_y || m_dim_z != other.m_dim_z) {
+    for (int x = 0; x < m_dim_x; ++x) {
+      for (int y = 0; y < m_dim_y; ++y) {
+        for (int z = 0; z < m_dim_z; ++z) {
+          if ((*this)(x, y, z) != other(x, y, z)) {
             return false;
+          }
         }
-
-        for (int x = 0; x < m_dim_x; ++x) {
-            for (int y = 0; y < m_dim_y; ++y) {
-                for (int z = 0; z < m_dim_z; ++z) {
-                    if ((*this)(x, y, z) != other(x, y, z)) {
-                        return false;
-                    }
-                }
-            }
-        }
-
-        return true;
+      }
     }
 
-    int length() const { return m_dim_x * m_dim_y * m_dim_z; }
-    int m_dim_x, m_dim_y, m_dim_z;
-    T* m_data;
+    return true;
+  }
 
-    // Default constructor
-    Matrix3D() { m_data = NULL; }
+  int length() const { return m_dim_x * m_dim_y * m_dim_z; }
+  T sum() const {
+    T sum = 0;
+    for (int i = 0; i < this->length(); i++) {
+      sum += this->m_data[i];
+    }
+    return sum;
+  }
+  T sum(int size) const {
+    T sum = 0;
+    for (int i = 0; i < size; i++) {
+      sum += this->m_data[i];
+    }
+    return sum;
+  }
+
+  T sum(int size, int start_idx) const {
+    T sum = 0;
+    for (int i = 0; i < size; i++) {
+      sum += this->m_data[start_idx + i];
+    }
+    return sum;
+  }
+
+  void load(const char *path) {
+    std::ifstream infile(path, std::ios::binary | std::ios::in);
+    if (infile.fail()) {
+      std::cout << "Failed to open file: " << strerror(errno) << std::endl;
+      throw("Expected error...");
+    } else {
+      infile.read(reinterpret_cast<char *>(this->m_data),
+                  this->length() * sizeof(T));
+      infile.close();
+    }
+  }
+  int m_dim_x, m_dim_y, m_dim_z;
+  T *m_data;
+
+  // Default constructor
+  Matrix3D() { m_data = NULL; }
 };
 
 static inline void debug_info(std::string s) {
 #ifdef DEBUG
-    std::cout << s << std::endl;
+  std::cout << s << std::endl;
 #endif
 }
 #endif
--- transformer/include/model.h
+++ transformer/include/model.h
@@ -11,54 +11,50 @@
 // #define MAXVOCSIZE 50272
 
 struct model_config {
-    int batch;
-    int num_heads;
-    int num_layers;
-    int max_sqlen;
-    int embed_dim;
-    int hidden_dim;
-    int vocsize;
-    int padding_idx;
+  int batch;
+  int num_heads;
+  int num_layers;
+  int max_sqlen;
+  int embed_dim;
+  int hidden_dim;
+  int vocsize;
+  int padding_idx;
 
-    model_config() : model_config(1, 12, 12, 512, 768, 3072, 50272, 1) {}
-    model_config(int batch, int num_heads, int num_layers, int max_sqlen, int embed_dim, int hidden_dim, int vocsize,
-                 int padding_idx)
-        : batch(batch),
-          num_heads(num_heads),
-          num_layers(num_layers),
-          max_sqlen(max_sqlen),
-          embed_dim(embed_dim),
-          hidden_dim(hidden_dim),
-          vocsize(vocsize),
-          padding_idx(padding_idx) {}
+  model_config() : model_config(1, 12, 12, 512, 768, 3072, 50272, 1) {}
+  model_config(int batch, int num_heads, int num_layers, int max_sqlen,
+               int embed_dim, int hidden_dim, int vocsize, int padding_idx)
+      : batch(batch), num_heads(num_heads), num_layers(num_layers),
+        max_sqlen(max_sqlen), embed_dim(embed_dim), hidden_dim(hidden_dim),
+        vocsize(vocsize), padding_idx(padding_idx) {}
 };
 
 enum {
-    OPT_125M,
-    OPT_1_3B,
-    OPT_6_7B,
+  OPT_125M,
+  OPT_1_3B,
+  OPT_6_7B,
 };
 
-const struct model_config opt_6_7B(1, 32, 32, 512, 4096, 16384, 50272, 1);
-const struct model_config opt_1_3B(1, 32, 24, 512, 2048, 8192, 50272, 1);
-const struct model_config opt_125m(1, 12, 12, 512, 768, 3072, 50272, 1);
+const struct model_config opt_6_7B(1, 32, 32, 2048, 4096, 16384, 50272, 1);
+const struct model_config opt_1_3B(1, 32, 24, 2048, 2048, 8192, 50272, 1);
+const struct model_config opt_125m(1, 12, 12, 2048, 768, 3072, 50272, 1);
+const struct model_config llama_7B(1, 32, 32, 2048, 4096, 4096, 50272, 1);
 static struct model_config get_opt_model_config(int choise) {
-    struct model_config ret;
-    switch (choise) {
-        case OPT_125M:
-            ret = opt_125m;
-            break;
-        case OPT_1_3B:
-            ret = opt_1_3B;
-            break;
-        case OPT_6_7B:
-            ret = opt_6_7B;
-            break;
-        default:
-            throw("Unsupported model choise.");
-            break;
-    }
-    return ret;
+  struct model_config ret;
+  switch (choise) {
+  case OPT_125M:
+    ret = opt_125m;
+    break;
+  case OPT_1_3B:
+    ret = opt_1_3B;
+    break;
+  case OPT_6_7B:
+    ret = opt_6_7B;
+    break;
+  default:
+    throw("Unsupported model choise.");
+    break;
+  }
+  return ret;
 }
 
 #endif
