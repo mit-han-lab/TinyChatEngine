8841033ff93d4871a9e87b907696f010f201d31b
meenchen <meenchen79@gmail.com>
2023-08-24 17:14:32 -0400

    minor

--- llm/application/chat.cc
+++ llm/application/chat.cc
@@ -82,7 +82,7 @@ int main(int argc, char* argv[]) {
         if (isLLaMA(target_model)) {
             std::cout << "Using model: " + target_model << std::endl;
             if (target_data_format == "INT4" || target_data_format == "int4")
-                std::cout << "Using data format: " << "AWQ" target_data_format << std::endl;
+                std::cout << "Using data format: " << "AWQ" << target_data_format << std::endl;
             else
                 std::cout << "Using data format: " << target_data_format << std::endl;
         } else {  // OPT
