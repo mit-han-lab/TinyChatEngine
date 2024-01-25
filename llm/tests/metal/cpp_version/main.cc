69c929d4f202f40ab7439b6430a509cd6ed9dca6
DerrickYLJ <lijiey@andrew.cmu.edu>
2024-01-25 10:58:14 -0500

    speedup issue with metal

--- llm/tests/metal/cpp_version/main.cc
+++ llm/tests/metal/cpp_version/main.cc
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <random>
 #include <ctime>
+#include <chrono>
 
 #define NS_PRIVATE_IMPLEMENTATION
 #define CA_PRIVATE_IMPLEMENTATION
@@ -15,7 +16,20 @@
 #include "Metal/Metal.hpp"
 #include "Foundation/Foundation.hpp"
 
-int arraySize = 100;
+MTL::Buffer* pM1;
+MTL::Buffer* pM2;
+MTL::Buffer* pM3;
+
+using namespace std;
+using namespace chrono;
+
+int arraySize = 100000;
+
+void addArrays(const int arr1[], const int arr2[], int result[], int size) {
+    for (int i = 0; i < size; ++i) {
+        result[i] = arr1[i] + arr2[i];
+    }
+}
 
 // Function to generate a random integer array
 void generateRandomIntArray(int* array) {
@@ -36,13 +50,24 @@ int main(){
 //    int M1[5][5], M2[5][5], Output[5][5];
     int *M1 = new int[arraySize];
     int *M2 = new int[arraySize];
-    int *Output = new int[arraySize];
+    int *M3 = new int[arraySize];
+
+    
     
     generateRandomIntArray(M1);
     generateRandomIntArray(M2);
-    generateRandomIntArray(Output);
+    generateRandomIntArray(M3);
+    std::cout << "M1[0]: " << M1[0] << " " << M1[1] << " " << M1[2] << std::endl;
+    std::cout << "M2[0]: " << M2[0] << " " << M2[1] << " " << M2[2] << std::endl;
+    std::cout << "M3[0]: " << M3[0] << " " << M3[1] << " " << M3[2]  << std::endl;
+
+
+    auto start2 = high_resolution_clock::now();
+    addArrays(M1, M2, M3, arraySize);
+    auto stop2 = high_resolution_clock::now();
+    auto duration2 = duration_cast<microseconds>(stop2 - start2);
     
-    
+    // auto start = high_resolution_clock::now();
     MTL::Device *_mDevice = MTL::CreateSystemDefaultDevice();
     NS::Error *error = nullptr;
     MTL::Library *defaultLibrary = _mDevice->newDefaultLibrary();
@@ -81,9 +106,16 @@ int main(){
     }
     
     //Create Metal buffers for input and output, if inside the TinyChat, param should be created in advance
-    MTL::Buffer *buffer1 = _mDevice->newBuffer(sizeof(M1), MTL::ResourceStorageModeShared);
-    MTL::Buffer *buffer2 = _mDevice->newBuffer(sizeof(M2), MTL::ResourceStorageModeShared);
-    MTL::Buffer *buffer3 = _mDevice->newBuffer(sizeof(Output), MTL::ResourceStorageModeShared);
+    MTL::Buffer *buffer1 = _mDevice->newBuffer(sizeof(int)*arraySize, MTL::ResourceStorageModeShared);
+    MTL::Buffer *buffer2 = _mDevice->newBuffer(sizeof(int)*arraySize, MTL::ResourceStorageModeShared);
+    MTL::Buffer *buffer3 = _mDevice->newBuffer(sizeof(int)*arraySize, MTL::ResourceStorageModeShared);
+    
+    pM1 = buffer1;
+    pM2 = buffer2;
+    pM3 = buffer3;
+    memcpy(pM1->contents(), M1, arraySize);
+    memcpy(pM2->contents(), M2, arraySize);
+    memcpy(pM3->contents(), M3, arraySize);
     
     // Start the computation in metal gpu
     // Create a command buffer to hold commands.
@@ -97,20 +129,28 @@ int main(){
     // Set buffers for input and output
     // Encode the pipeline state object and its parameters.
     computeEncoder->setComputePipelineState(_mMatmulFunctionPSO);
-    computeEncoder->setBuffer(buffer1, 0, 0);
-    computeEncoder->setBuffer(buffer2, 0, 1);
-    computeEncoder->setBuffer(buffer3, 0, 2);
+    computeEncoder->setBuffer(pM1, 0, 0);
+    computeEncoder->setBuffer(pM2, 0, 1);
+    computeEncoder->setBuffer(pM3, 0, 2);
     
     // number of threadgroup
-    uint32_t maxThreadsperthreadgroup = (uint32_t)_mMatmulFunctionPSO->maxTotalThreadsPerThreadgroup();
-    uint32_t threadsPerthreadgroup = MIN(maxThreadsperthreadgroup, arraySize);
-    MTL::Size threadgroupCount = MTL::Size::Make((arraySize+threadsPerthreadgroup-1)/threadsPerthreadgroup, 1, 1);
-
+    MTL::Size mtlthreadsPerthreadgroup = MTL::Size::Make(arraySize, 1, 1);
     // Calculate a thread number per group
-    MTL::Size threadgroupSize = MTL::Size::Make(threadsPerthreadgroup, 1, 1);
+    MTL::Size threadgroupSize = MTL::Size::Make(1, 1, 1);
 
+
+    // Set threadgroup size and dispatch compute threads
+    NS::UInteger maxThreadsperthreadgroup = _mMatmulFunctionPSO->maxTotalThreadsPerThreadgroup();
+    NS::UInteger threadsPerThreadgroup = MIN(arraySize, maxThreadsperthreadgroup);
+    MTL::Size threadgroupCount = MTL::Size::Make((arraySize + threadsPerThreadgroup - 1) / threadsPerThreadgroup, 1, 1);
+    // Dispatch threads in multiple threadgroups
+    MTL::Size threadgroups = MTL::Size::Make(threadsPerThreadgroup, 1, 1);
+    
+
+
+    auto start = high_resolution_clock::now();
     // Encode the compute command.
-    computeEncoder->dispatchThreads(threadgroupCount, threadgroupSize);
+    computeEncoder->dispatchThreads(mtlthreadsPerthreadgroup, threadgroupSize);
 
     // End the compute pass.
     computeEncoder->endEncoding();
@@ -122,11 +162,20 @@ int main(){
     // but in this example, the code simply blocks until the calculation is complete.
     commandBuffer->waitUntilCompleted();
     
-    int *output = (int*)buffer3->contents();
-    std::cout << "The output from Metal GPU is: " << output[0] << std::endl;
+    // std::cout << "M1[0]: " << ((int*)(buffer1->contents()))[0] << " " << ((int*)(buffer1->contents()))[1] << " " << ((int*)(buffer1->contents()))[2] << std::endl;
+    // std::cout << "M2[0]: " << ((int*)(buffer2->contents()))[0] << " " << ((int*)(buffer2->contents()))[1] << " " << ((int*)(buffer2->contents()))[2] << std::endl;
+    // std::cout << "M3[0]: " << ((int*)(buffer3->contents()))[0] << " " << ((int*)(buffer3->contents()))[1] << " " << ((int*)(buffer3->contents()))[2]  << std::endl;
 
+    
     computeEncoder->release();
     commandBuffer->release();
-    
-}
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    cout << "GPU: " << duration.count() << " microseconds" << endl;
+    cout << "CPU: " << duration2.count() << " microseconds" << endl;
+}   
+
+
+
 
--- llm/tests/metal/cpp_version/op.metal
+++ llm/tests/metal/cpp_version/op.metal
@@ -8,13 +8,13 @@
 #include <metal_stdlib>
 using namespace metal;
 
-kernel void arrayAdd(const device float* inputA [[buffer(0)]],
-                       const device float* inputB [[buffer(1)]],
-                       device float* output [[buffer(2)]],
+kernel void arrayAdd(const device int* inputA,
+                       const device int* inputB,
+                       device int* output,
                        uint id [[thread_position_in_grid]])
  {
-     // Perform array addition
-     output[id] = inputA[id] + inputB[id];
+    // Perform array addition
+    output[id] = inputA[id] + inputB[id];
  }
 
 
