69407d528c4853a478eaec4f115e379a8b18c3d8
DerrickYLJ <lijiey@andrew.cmu.edu>
2024-01-27 19:34:54 -0500

    header param

---
+++ llm/tests/metal/cpp_version/param.h
@@ -0,0 +1,15 @@
+//
+//  param.h
+//  metal_cpp
+//
+//  Created by Derrick on 1/27/24.
+//
+
+#ifndef param_h
+#define param_h
+
+
+#endif /* param_h */
+typedef struct  {
+    unsigned int width1, height1, width2, height2, outputsize, arraySize1, arraySize2;
+}matmul_param;
